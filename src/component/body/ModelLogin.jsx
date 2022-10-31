4afea33709676586e31b8616b33a3c574751a484
Mohammed@almallahi98 <x52-@hotmail.com>
2022-10-31 10:55:35 +0300

    adding functionalty to login model using useState with a bug in setValid

--- src/component/body/ModelLogin.jsx
+++ src/component/body/ModelLogin.jsx
@@ -12,7 +12,7 @@ import {
     ModalCloseButton,
   } from '@chakra-ui/react';
   import { useDisclosure } from '@chakra-ui/react';
-  import React from 'react';
+  import React, { useState } from 'react';
   
 
 function ModelLogin() {
@@ -20,6 +20,23 @@ function ModelLogin() {
   
     const initialRef = React.useRef(null)
     const finalRef = React.useRef(null)
+    const [User,setUser]=useState();
+    const[Password,setPassword]=useState();
+    const[Valid,setValid]=useState();
+    const validatior=()=>{
+        if(User.length!== 0 && Password.length!== 0)
+        {
+            setValid(true);
+            console.log(Valid);
+        }
+        else
+        {
+            setValid(false);
+            console.log(Password);
+        }
+    }
+
+
   
     return (
       <>
@@ -40,21 +57,22 @@ function ModelLogin() {
             <ModalBody pb={6}>
               <FormControl>
                 <FormLabel>User Name</FormLabel>
-                <Input type={"email"} ref={initialRef} placeholder='Email' />
+                <Input type={"email"} onChange={(e)=>{setUser(e.target.value)}} ref={initialRef} placeholder='Email' />
               </FormControl>
   
               <FormControl mt={4}>
                 <FormLabel>Password</FormLabel>
-                <Input type={"password"} placeholder='Password' />
+                <Input type={"password"} onChange={(e)=>{setPassword(e.target.value)}} placeholder='Password' />
               </FormControl>
             </ModalBody>
   
             <ModalFooter>
-              <Button colorScheme='green' mr={3}>
+              <Button colorScheme='green' mr={3} onClick={validatior} >
                 sign in
               </Button>
               <Button onClick={onClose}>Cancel</Button>
             </ModalFooter>
+            <p>{Valid}</p>
           </ModalContent>
         </Modal>
       </>
